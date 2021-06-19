0b1d1d61357cf915ac63aa031cc81d89b22df991
Loganathan M <Loganathan_M3@Dellteam.com>
2021-06-19 12:03:36 +0530

    Adding Worker class with custom delegates

---
+++ DeleagtesEventsAndLambdas/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DeleagtesEventsAndLambdas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var worker = new Worker();
+            worker.
+        }
+    }
+}
---
+++ DeleagtesEventsAndLambdas/Worker.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleagtesEventsAndLambdas
+{
+    public delegate void WorkPerfomedHandler(object sender, WorkPerformedEventArgs args);
+
+    public class Worker
+    {
+        //Below approach is used to create events using in-build delegate - EventHandler and
+        //for this no explicit delegate declaration is not required. 
+        //public event EventHandler<WorkPerformedEventArgs> WorkPerformed1;
+        public event WorkPerfomedHandler WorkPerfomed;
+        public event EventHandler WorkCompleted;
+
+        public void DoWork(int hours, WorkType workType)
+        {
+            for (int i = 0; i < hours; i++)
+            {
+                OnWorkPerformed(i+1, workType);
+            }
+            OnWorkCompleted();  
+        }
+
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+            //First approach to rasie event by directly calling the declared event
+            //if (WorkPerfomed != null)
+            //{
+            //    WorkPerfomed(hours, workType);
+            //}
+
+            //Second approach to rasie event by using the delegate
+            var del = WorkPerfomed as WorkPerfomedHandler;
+            if (del != null)
+            {
+                del(this, new WorkPerformedEventArgs(hours, workType));
+            }
+
+        }
+
+        protected virtual void OnWorkCompleted()
+        {
+            //First approach to rasie event by directly calling the declared event
+            //if (WorkCompleted != null)
+            //{
+            //    WorkCompleted(this, EventArgs.Empty);
+            //}
+
+            //Second approach to rasie event by using the delegate
+            var del = WorkCompleted;
+            if (del != null)
+            {
+                del(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public enum WorkType
+    {
+         Design,
+         Develop, 
+         Test,
+         Deploy
+    }
+}
